87341a62fb067858231607950f35903862177d15
COSMIC <rayavanindra@gmail.com>
2024-11-07 03:58:14 +0530

    PIHooks: Update komodo fingerprint to November release

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -86,8 +86,8 @@ public class PropImitationHooks {
             "MANUFACTURER", "Google",
             "BRAND", "google",
             "MODEL", "Pixel 9 Pro XL",
-            "ID", "AP3A.241005.015",
-            "FINGERPRINT", "google/komodo/komodo:15/AP3A.241005.015/12366759:user/release-keys"
+            "ID", "AP3A.241105.008",
+            "FINGERPRINT", "google/komodo/komodo:15/AP3A.241105.008/12485168:user/release-keys"
     );
 
     private static final Map<String, String> sPixelFiveProps = Map.of(
