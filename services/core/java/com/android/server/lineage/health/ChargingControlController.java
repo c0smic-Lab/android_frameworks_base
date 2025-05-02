d8f6e955493071b8f0327c24f15fbdd74c780c18
Trogel <trogel@clink.de>
2025-05-02 10:49:50 +0000

    health: Don't turn on charging control if not plugged in
    Also cancel a possibly existing charging control notification.
    
    Previously charging control was turned on when settings were changed and
    on system start even if no charger was plugged in, thus possibly showing
    the charging control notification.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/8610
    Change-Id: I78c47f026aa47ac756cc6eca12157ea58bda58fd

--- services/core/java/com/android/server/lineage/health/ChargingControlController.java
+++ services/core/java/com/android/server/lineage/health/ChargingControlController.java
@@ -406,8 +406,9 @@ public class ChargingControlController extends LineageHealthFeature {
     }
 
     protected void updateChargeControl() {
-        if (!isEnabled() || mIsControlCancelledOnce) {
+        if (!isEnabled() || mIsControlCancelledOnce || !mIsPowerConnected) {
             mCurrentProvider.disable();
+            mChargingNotification.cancel();
             return;
         }
 
